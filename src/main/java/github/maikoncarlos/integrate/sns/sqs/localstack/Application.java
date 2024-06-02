947283929fdbb5f51b64f6b609c5a384cd397be6
Maikoncarlos <maikonsaturno@gmail.com>
2024-06-01 21:21:14 -0300

    first commit

---
+++ src/main/java/github/maikoncarlos/integrate/sns/sqs/localstack/Application.java
@@ -0,0 +1,13 @@
+package github.maikoncarlos.integrate.sns.sqs.localstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/github/maikoncarlos/integrate/sns/sqs/localstack/ApplicationTests.java
@@ -0,0 +1,13 @@
+package github.maikoncarlos.integrate.sns.sqs.localstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
